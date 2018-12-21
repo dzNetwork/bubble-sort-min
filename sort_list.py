ca5ef7f5770c5b64d2561f2ed4de787c43fe0fb3
dznet <dznet@dznet.host>
2018-12-21 20:24:26 +0200

    prettyfi

--- sort_list.py
+++ sort_list.py
@@ -6,36 +6,35 @@ DIGITS_LIST = [3, 7, 1, 5, 0, 2, 9, 6]
 RANDOM_LIST = [choice(digits) for _ in range(24)]
 
 
-# v1
+# v1-dznet
 
 def sort_digits_list(_list):
 
-  unit = 1
+  _int = int(1)
   key = int()
-  length = (_list.__len__() - unit)
 
-  for _ in range(length):
+  for _ in _list:
 
     for digit in _list:
 
-      key = key.__add__(unit)
+      key = key.__add__(_int)
 
-      if key.__lt__(length + unit) \
+      if key.__lt__(_list.__len__()) \
       and digit.__gt__(_list.__getitem__(key)):
 
         tmp = _list.__getitem__(key)
 
         _list.__setitem__(key, digit)
-        _list.__setitem__(key - unit, tmp)
+        _list.__setitem__((key - _int), tmp)
 
     key = int()
 
   return _list
 
 
-# v2
+# v2-buildin
 
-def sort_digits_list(_list):
+def buildin_sort(_list):
   return _list.sort()
 
 
